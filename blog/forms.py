1911971262c4419587d555a477ea25bc5d7954dc
Simon Recofsky <Russorecof@gmail.com>
2024-08-20 18:33:19 -0300

    Segunda tanda de ajustes

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import Blog
-
-
+from django import forms
+from .models import Blog
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ['title', 'subtitle', 'body', 'author', 'image']
+        fields = ['title', 'subtitle', 'body', 'author', 'date_created', 'image']
 
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 4, 'cols': 40}),  # Ajusta el tamaño del área de texto si es necesario
-            'image': forms.ClearableFileInput(),  # Usa el widget predeterminado para una sola imagen
+            'body': forms.Textarea(attrs={'rows': 4, 'cols': 40}), 
+            'image': forms.ClearableFileInput(),
         }
--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    subtitle = models.CharField(max_length=800, blank=True, null=True)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_created = models.DateTimeField(default=timezone.now)  
+    date_created = models.DateField(default=timezone.now) 
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
     
     def __str__(self):
